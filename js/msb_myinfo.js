0cdb2514750e33bd96978a3c0186bb6f2c608d68
pengmiao <pengmiao@163.com>
2024-08-28 14:08:31 +0800

    ...

--- js/msb_myinfo.js
+++ js/msb_myinfo.js
@@ -1,5 +1,6 @@
 var obj = JSON.parse($response.body);
 obj.data.vipFlag = 1
+obj.data.isVip = true
 obj.data.isQaTeacher = true
 obj.data.isAdvisorTeacher = true
 $done({ body: JSON.stringify(obj) });
